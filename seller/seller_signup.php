e0995dfb1c0d24e7fac926c9dad69825ab78a8b2
nahi3an <nafisnahiyan321@gmail.com>
2021-11-08 04:28:42 +0600

    seller signup form created

--- seller/seller_signup.php
+++ seller/seller_signup.php
@@ -1,16 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+include_once '../header.php';
 
-<body>
+?>
+
+<div class="seller-signup">
+    <br><br><br>
+    <form action="/online_shopping_system/includes/seller_inc/seller_signup_inc.php" method="POST">
+        <label>Enter Name:</label><br>
+        <input type="text" name="seller_name"> <br>
+        <label>Enter Email:</label><br>
+        <input type="text" name="seller_email"><br>
+        <label>Enter Password:</label><br>
+        <input type="password" name="seller_password"> <br>
+        <label>Confirm Password:</label><br>
+        <input type="password" name="seller_password_confirm">
+        <br>
+        <input type="submit" value="Sign Up" name="seller_signup"><br>
+    </form>
+</div>
 
 
-</body>
+<?php
 
-</html>
+include_once '../footer.php';
+
+?>
